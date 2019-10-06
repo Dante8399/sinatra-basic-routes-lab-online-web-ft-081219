905da8c0ceb70aa370c13a9cc7b2566539e2f3f2
Dante Spencer <spencerdantel@gmail.com>
2019-10-06 14:01:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,5 +5,17 @@ class App < Sinatra::Base
     "Hello, World!"
   end
   
+  get '/' do
+    "My name is_"
+  end
+  
+  get '/' do
+    "My hometown is_"
+  end
+  
+  get '/' do
+    "My favorite song is_"
+  end
+  
   
 end
